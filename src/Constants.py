7523c0784610ff9b9ae174050743ad8f53578699
wu allen <cooolallen1@wudeMacBook-Pro.local>
2018-11-30 11:57:40 -0500

    email function complete

--- src/Constants.py
+++ src/Constants.py
@@ -5,3 +5,11 @@ updateRate = 150					# update rate in ms
 consecutiveFactor = 5				# when predict is more than consecutiveFactor, we set the real prediction as curr_prediction
 goodPosture = {0, 1, 2}				# the value of the normal posture
 alarmParams = {'phone':1000, 'pop_out': 100, 'vibrate': 10}		# the peroid of each notification (unit: timestamp)
+
+
+# constants related to email
+defaultParas = {}
+defaultParas['password'] = 'zerodudepublic'
+defaultParas['sender'] = 'publicdudezero@gmail.com'
+defaultParas['destination'] = 'swimm1rsu@gmail.com'
+defaultParas['subject'] = 'Posture alarm'
--- src/Top.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from UI import MainWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    mainWindow = MainWindow()
-
-    sys.exit(app.exec_())
