6ef2621369659b0d1c2e7fa02baa424cb92f1a54
wu allen <cooolallen1@wudeMacBook-Pro.local>
2018-12-03 11:24:12 -0500

    training data updated

--- src/Constants.py
+++ src/Constants.py
@@ -1,11 +1,11 @@
 IMG_DIR = '../images'
 DATA_DIR = '../data'			
-test = True			
+test = False			
 normalize = False			
 updateRate = 150					# update rate in ms
 consecutiveFactor = 5				# when predict is more than consecutiveFactor, we set the real prediction as curr_prediction
 goodPosture = {0, 1, 2}				# the value of the normal posture
-alarmParams = {'phone':1000, 'pop_out': 100, 'vibrate': 10}		# the peroid of each notification (unit: timestamp)
+alarmParams = {'phone':150, 'pop_out': 100, 'vibrate': 10}		# the peroid of each notification (unit: timestamp)
 
 
 # constants related to email
